3c276e5ef66c5857516f94807a649ffd4fac49b7
Bernatbgg9 <bernatbgg9@gmail.com>
2023-11-08 16:21:24 +0100

    brrrr

---
+++ src/bullet.lua
@@ -0,0 +1,16 @@
+Actor = Actor or require "src/actor"
+local Bullet = Actor:extend()
+
+function Bullet:new(x, y)
+
+end
+
+function Bullet:update(dt)
+
+end
+
+function Bullet:draw()
+
+end
+
+return Bullet
