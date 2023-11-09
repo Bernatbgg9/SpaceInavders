63e658f6621511cb91add2dc68ec9059306da31d
Bernatbgg9 <bernatbgg9@gmail.com>
2023-11-09 17:34:31 +0100

    brrr

--- src/Bala.lua
+++ src/Bala.lua
@@ -1,16 +1,31 @@
-Actor = Actor or require "src/actor"
-local Bala = Actor:extend()
+local Actor = Actor or require "Scripts/actor"
+Bala = Actor:extend()
 
-function Bala:new(x, y)
-
+function Bala:new()
+  Bala.super.new(self, "src/textures/balaMalo.png", 1, 1, 500, 1, 0)
 end
 
 function Bala:update(dt)
-
+ --Bala.super.update(self, dt)
+  self.position.y = self.position.y + self.speed * dt
+  if self.position.x >= w or self.position.x < 0 or self.position.y >= h or self.position.y < 0 then
+    for i, v in pairs(actorList) do
+      if (v == self) then
+        table.remove(actorList, i)
+      end
+    end
+  end
 end
 
 function Bala:draw()
-
+  local xx = self.position.x
+  local ox = self.origin.x
+  local yy = self.position.y
+  local oy = self.origin.y
+  local sx = self.scale.x
+  local sy = self.scale.y
+  local rr = self.rot
+  love.graphics.draw(self.image, xx, yy, rr, sx, sy, ox, oy, 0, 0)
 end
 
 return Bala
--- src/Enemi.lua
+++ src/Enemi.lua
@@ -1,12 +1,15 @@
 Actor = Actor or require "src/actor"
 local Enemi = Actor:extend()
+local Bala = Bala or require "src/Bala"
+
+
 
 local stop = true
 
-local skinMalos = {"src/textures/malo1.png","src/textures/malo.png","src/textures/malo2.png"}
+local skinMalos = { "src/textures/malo1.png", "src/textures/malo.png", "src/textures/malo2.png" }
 
 function Enemi:new(x, y)
-    Enemi.super.new(self, skinMalos[math.random(1,3)], 30, 30, 50, 1, 0)
+    Enemi.super.new(self, skinMalos[math.random(1, 3)], 30, 30, 50, 1, 0)
     self.position.x = 30
     self.position.y = 30
     self.speed      = 250
@@ -46,6 +49,25 @@ function Enemi:update(dt)
             self.position.y = self.position.y + self.speed * dt
         elseif self.position.y > 390 then
             stop = false
+        end
+    end
+    local enemy = {}
+
+    for k, v in ipairs(actorList) do
+        if v:is(Enemi) then
+            table.insert(enemy, Enemi)
+            local num = math.random(1, #enemy)
+
+            print(1)
+            for k, v in ipairs(enemy) do
+                if k == num then
+                    local bala = Bala()
+                    bala.position.x = self.position.x
+                    bala.position.y = self.position.y
+                    table.insert(actorList, bala)
+                    print(2)
+                end
+            end
         end
     end
 end
